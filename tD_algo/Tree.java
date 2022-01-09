0b62b3da7da80dcd67639f76ba63a52932704514
EliottDelannay <eliott@delannay.fr>
2022-01-09 21:43:27 +0100

    ex1

--- tD_algo/Tree.java
+++ tD_algo/Tree.java
@@ -4,8 +4,27 @@ public class Tree {
 	private Node root;
 	
 	
-	private void buildTree() {
+	private void buildTree(List Node) {
 		
+	}
+	
+	private void buildStaticTree{
+	    Node a = new Node('a');
+	    Node b = new Node('b');
+	    Node c = new Node('c', false);
+	    Node d = new Node('d');
+	    Node e = new Node('e');
+	    Node f = new Node('f', false);
+	    Node g = new Node('g');
+	    Node h = new Node('h');
+	    Node i = new Node('i');
+	    Node j = new Node('j', false);
+	    Node k = new Node('k', false);
+	    Node l = new Node('l', false);
+	    Node m = new Node('m');
+	    Node n = new Node('n', false);
+	    Node o = new Node('o', false);
+	    Node p = new Node('p',false);
 	}
 	
 	/* EX2
