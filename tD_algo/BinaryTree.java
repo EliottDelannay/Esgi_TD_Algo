3554bdec3abe70e91dc94d3f2f00bcb8cf91d5dd
EliottDelannay <eliott@delannay.fr>
2022-01-09 20:24:14 +0100

    project

---
+++ tD_algo/BinaryTree.java
@@ -0,0 +1,83 @@
+package tD_algo3;
+
+public class BinaryTree {
+	Node root;
+	
+	private Node addRecursive(Node current, int value) {
+		if (current == null) {
+			return new Node(value);
+		}
+		
+		if (value < current.value) {
+			//add the lower value to the left side
+			current.left = addRecursive(current, value);
+		}
+		else if(value > current.value) {
+			//add the upper value to the right side 
+			current.right = addRecursive(current,value);
+		} else {
+			return current;
+		}
+		
+		return current;
+	}
+	
+	public void add(int value) {
+		root = addRecursive(root, value);
+	}
+	
+	private BinaryTree createStaticBinaryTree() {
+		BinaryTree bt = new BinaryTree();
+		
+		bt.add(20);
+		bt.add(5);
+		bt.add(25);
+		bt.add(3);
+		bt.add(12);
+		bt.add(21);
+		bt.add(28);
+		bt.add(8);
+		bt.add(13);
+		bt.add(6);
+		bt.add(19);
+		bt.add(50);
+		
+		return bt;
+	}
+	
+	// parcours infixe (première valeur de la node -> node gauche  -> node droit)
+	public void traversePreOrder(Node node) {
+	    if (node != null) {
+	        System.out.print(" " + node.value);
+	        traversePreOrder(node.left);
+	        traversePreOrder(node.right);
+	    }
+	}
+	
+	/*EX2
+	 
+	 private generateRandomList(int size)
+	 {
+	 	List r;
+	 	for ( i++ < size) {
+	 		r.i.(random);
+	 	}
+	 	return r;
+	 }
+	 
+	 private BinaryTree createBinaryTreeFromList(list random){
+	 	BinaryTree bt = new BinaryTree();
+	 	
+	 	while(i < random.size) {
+		 	bt.add(random.i);
+		 	i += 1;
+	 	}
+	 	return bt;
+	 }
+	  
+	 
+	  
+	 */
+	
+}
+
--- tD_algo/Node.java
+++ tD_algo/Node.java
@@ -1,17 +1,14 @@
-package tD_algo;
+package tD_algo3;
 
 public class Node {
 	int value;
-	private boolean isNode;  
+	Node left;
+	Node right;
 	
 	Node(int value){
 		this.value = value;
-		this.isNode = true;
-	}
-	
-	Node(int value, boolean isNode) {
-		this.value = value;
-		this.isNode = isNode;
+		right = null;
+		left = null;
 	}
 
 }
