de7d4a56312b3366e07852cfa8ed18300f63dd0c
EliottDelannay <eliott@delannay.fr>
2022-02-10 16:23:39 +0100

    Suppression test dans main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Graphe graphe = new Graphe();
-        //matrice d'adjacence (matrice carré) entre plusieurs sommets
+
         Integer[][] matrice = {
                 {0,1,0},
                 {1,0,1},
@@ -15,11 +15,6 @@ public class Main {
         String[] noms = {"Lyon", "Bayonne", "Paris"};
 
         graphe.setGraphe(noms, matrice);
-
-        // Affiche le nombre de sommets adjacents au sommet X
-        System.out.println(graphe.getSommet(0).getSommetsAdjacents().size());
-        System.out.println(graphe.getSommet(1).getSommetsAdjacents().size());
-        System.out.println(graphe.getSommet(2).getSommetsAdjacents().size());
 
         List<Sommet> cheminInverse = new ArrayList<>();
         System.out.println("Un chemin existe? " + Graphe.parcoursProfondeurModifie(graphe.getSommet(0), graphe.getSommet(2), cheminInverse, new ArrayList<>()));
@@ -34,20 +29,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        // Affiche "Ecrire un nombre: "
-        System.out.println("Ecrire un nombre: ");
-
-        //Lit l'entier écrit par l'utilisateur et l'affecte
-        // à  la variable nombre du programme
-        int nombre = scanner.nextInt();
-        // Affiche le nombre saisit par l'utilisateur
-        System.out.println(nombre);
-
-
         List<SommetDijkstra> list = Graphe.calculateShortestPath(graphe,graphe.getSommet(0) );
-        for (SommetDijkstra d: list) {
-            System.out.println(d.sommet.getNom());
-        }
 
     }
 
